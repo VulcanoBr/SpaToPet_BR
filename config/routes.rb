e7da3762ff87a23ab0e83fd335c8b2b5ed5b0e6e
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-02-22 12:45:18 -0500

    add password controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :authentication, as: '', path: '' do
     resources :users, only: [:new, :create, :edit, :update], path: '/register', path_names: { new: '/' }
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+    resources :passwords, only: [:edit, :update], path: '/password', path_names: { edit: '/' }
   end
   get "up" => "rails/health#show", as: :rails_health_check
   root to: 'home#welcome'
