4181164198bcb246e91458d7a72a1bff7e4395c3
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-03-14 10:52:28 -0500

    Normalize pet name

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,5 +4,6 @@ class Pet < ApplicationRecord
   has_many :appointments
   has_rich_text :important_data
   validates :name, :breed, :kind, presence: { message: :blank }
+  normalizes :name, :breed, with: ->(value) { value.downcase }
   enum kind: { dog: 0, cat: 1, bird: 2, fish: 3, other: 4 }
 end
